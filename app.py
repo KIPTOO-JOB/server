4d3a3c7de4fc4e2e54668cb7df4d9a1da1439ce4
KIPTOO-JOB <job3kiptoo@gmail.com>
2024-10-22 12:46:22 +0300

    env

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URI')
 app.config['JWT_SECRET_KEY'] = os.environ.get ('JWT_SECRET_KEY')
 
 
